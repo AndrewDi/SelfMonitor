5b81381e40d06962cb8832fb5555f96d145b72ff
Andrew <pandipd@outlook.com>
2018-05-15 17:00:17 +0800

    Add basic function to collector

---
+++ Script/test.curl.sh
@@ -0,0 +1,1 @@
+curl -d "{\"type\":\"Data\",\"topic\":\"testData\",\"Data\":[{\"CollectTime\":\"2018-05-05-04.04.00\",\"PhysicHost\":\"fzcsDB\",\"ParentKey\":\"system\",\"Key\":\"filesystems\",\"Value\":50}]}" http://localhost:11111 
